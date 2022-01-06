4005f351c8d0c46f04b402370ac29243b40393c8
AlekseiSokolov20 <96689473+AlekseiSokolov20@users.noreply.github.com>
2022-01-06 11:27:08 +0300

    Added TableTests

--- src/test/java/ru/company/qa/SearchProductTests.java
+++ src/test/java/ru/company/qa/SearchProductTests.java
@@ -3,6 +3,7 @@ package ru.company.qa;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -10,6 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -24,8 +26,8 @@ public class SearchProductTests {
         // развёртывание окна до максимальной ширины экрана
         driver.manage().window().maximize();
         // ожидание открытия окна
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        //  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         // WebDriverWait wait = new WebDriverWait(driver, 30);
     }
 
@@ -40,6 +42,18 @@ public class SearchProductTests {
         String text = driver.findElement(By.className("lighter")).getText();
         Assert.assertEquals(text.toLowerCase(Locale.ROOT), "\"summer dresses\"");
     }
+
+    @Test
+    public void searchInContainerMenuTest(){
+        //перейти на первый элемент из списка меню "menu content"
+
+        WebElement ul = driver.findElement(By.xpath("//ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']"));
+        ul.findElement(By.xpath(".//a[@title=\"Women\"]")).click();
+
+        WebElement el = driver.findElement(By.xpath("//ul[@class=\"tree dynamized\"]"));
+        el.findElement(By.xpath(".//*[contains(text(),'Tops')]")).click();
+    }
+
 
     @AfterMethod(enabled = false)
     public void tearDown(){
